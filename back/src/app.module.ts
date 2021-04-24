025167b19abd985cc97610e02c07bc32f41485f7
John Tcha <johntcha94@gmail.com>
2021-04-24 12:20:10 +0200

    simple back authentification done

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NameModule } from './name/name.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [NameModule],
+  imports: [NameModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
