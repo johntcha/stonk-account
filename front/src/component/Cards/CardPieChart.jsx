0bad12e86788ff6961e7c664ba2fe4cf45c861ce
John Tcha <johntcha94@gmail.com>
2021-05-01 00:53:34 +0200

    tabs added to cards

--- front/src/component/Cards/CardPieChart.jsx
+++ front/src/component/Cards/CardPieChart.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import './card.css'
 
 const CardPieChart = ({
 
 }) => {
+    const [value, setValue] = useState(0);
+
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <Card className="card piechart">
+            <Tabs
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={handleTabChange}
+            aria-label="disabled tabs example"
+            >
+            <Tab label="Active" />
+            <Tab label="Active" />
+            </Tabs>
             <CardContent>
                 <Typography>
                     chart
--- front/src/component/Cards/CardTable.jsx
+++ front/src/component/Cards/CardTable.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import moment from 'moment'
 import ClearIcon from '@material-ui/icons/Clear';
 import IconButton from '@material-ui/core/IconButton';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import './card.css'
 
 const CardTable = ({
     expensesList,
     onClickDelete
 }) => {
+    const [value, setValue] = useState(0);
+
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <Card className="card whatever">
+            <Tabs
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={handleTabChange}
+            aria-label="disabled tabs example"
+            >
+            <Tab label="Active" />
+            <Tab label="Active" />
+            </Tabs>
             <CardContent>
                 <table className="expenses-list">
                 <thead>
@@ -28,9 +45,10 @@ const CardTable = ({
                         <td>{value.amount}</td>
                         <td>{value.currency}</td>
                         <td>
-                            <IconButton aria-label="clear" onClick={() => {onClickDelete(value.id);
-                            console.log(value.id)}}>
-                                <ClearIcon />
+                            <IconButton 
+                            aria-label="clear" 
+                            onClick={() => onClickDelete(value.id)}>
+                                <ClearIcon color="action" />
                             </IconButton>
                         </td>
                     </tr>  
