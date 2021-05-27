7b5b46280956c091341da0ad95a05c2dce0f7f63
John Tcha <johntcha94@gmail.com>
2021-05-27 16:40:10 +0200

    converting fieldinput into tsx file

---
+++ front/src/component/FieldInput.tsx
@@ -0,0 +1,30 @@
+import React, { ChangeEventHandler } from 'react'
+
+interface FieldInputProps {
+    label: string
+    type: string
+    placeholder: string
+    handleChange: ChangeEventHandler<HTMLInputElement>
+}
+
+const FieldInput = ({
+    label,
+    type,
+    placeholder,
+    handleChange
+}: FieldInputProps) => {
+    return (
+        <>
+        <label>
+            {label}
+        </label>
+        <input
+            type={type}
+            placeholder= {placeholder}
+            onChange={handleChange}
+        />
+        </>
+    )
+}
+
+export default FieldInput
