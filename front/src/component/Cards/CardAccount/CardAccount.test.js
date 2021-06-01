706cb0852b86216e0dbed7c43d3faa5aa523c67f
John Tcha <johntcha94@gmail.com>
2021-06-01 18:02:12 +0200

    quickfix CardAccount.test

--- front/src/component/Cards/CardAccount/CardAccount.test.js
+++ front/src/component/Cards/CardAccount/CardAccount.test.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import CardAccount from './CardAccount'
 import { render, screen, cleanup } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 describe('CardAccount', () => {
     afterEach(cleanup)
-    
-    it('should have the currency after the total amount at initial state', () => {
+
+    it('should have the currency after the total amount', () => {
         render(<CardAccount total={25} currency="€" />)
         expect(screen.getByText(/€/i)).toBeInTheDocument()
     })
