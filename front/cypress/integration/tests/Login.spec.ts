e7f98742c751c7ebbc986e885e0fea10ef9b173a
John Tcha <johntcha94@gmail.com>
2021-06-02 19:42:36 +0200

    adding e2e test to Login

--- front/cypress/integration/tests/Login.spec.ts
+++ front/cypress/integration/tests/Login.spec.ts
@@ -20,4 +20,13 @@ describe('login', () => {
         cy.url().should('eq', 'http://localhost:3000/');
     })
 
+    it('should open/close sign up pop', () => {
+        cy.visit('/login');
+        cy.get('input').first().type('john')
+        cy.get('form>input').eq(1).type('tcha')
+		cy.contains('Sign up').click()
+        cy.get('.signup-form').should('be.visible');
+        // cy.get('.signup-form button').first().click();
+        // cy.get('.signup-form').should('not.be.visible');
+	});
 })
