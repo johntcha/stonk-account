316ddf51f3ee70b519e5ae15af2693898db40d19
John Tcha <johntcha94@gmail.com>
2021-06-02 16:17:23 +0200

    removing examples + adding E2E test for Login

--- front/cypress/integration/tests/Login.spec.ts
+++ front/cypress/integration/tests/Login.spec.ts
@@ -1,6 +1,13 @@
 describe('login', () => {
-	it('should redirect to /login', ()=> {
+	it('should redirect to /login', () => {
 		cy.visit('/');
 		cy.url().should('include', '/login');
 	});
+
+    it('should not log in with empty credentials', () => {
+        cy.visit('/login');
+		cy.contains('Sign in').click();
+        cy.url().should('include', '/login');
+    })
+
 })
