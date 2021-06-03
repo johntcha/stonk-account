397ba577ea05d2e26e0fec2343a927185a7399f6
John Tcha <johntcha94@gmail.com>
2021-06-03 17:49:17 +0200

    fixing local storage save token e2e command + finishing Header e2e test

--- front/cypress/integration/tests/Header.spec.ts
+++ front/cypress/integration/tests/Header.spec.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('login', () => {
+	//We only log in once before running all the test
 	before(() => {
 		cy.intercept('GET', `http://localhost:3002/expense`).as('login');
 		cy.visit('/login');
@@ -8,14 +9,31 @@ describe('login', () => {
         cy.get('form>input').eq(1).type('tcha')
 		cy.contains('Sign in').click()
 		cy.wait('@login');
-		cy.saveLocalStorage();
 	})
-    beforeEach(() => {
-    	cy.visit('/');
-    });
 
-	it('should login', () => {
-		cy.get('[aria-label="logout-button"]>span').should('contain', 'Log Out');
+	beforeEach(() => {
+		cy.restoreLocalStorage();
+		cy.visit('/');
+	});
+	  
+	afterEach(() => {
+		cy.saveLocalStorage();
 	});
 
+	it("should display the user's username", () => {
+		cy.get('.header').should('contain', 'John')
+	})
+
+	it('should show the Log Out button on both desktop and mobile view', () => {
+		cy.get('[aria-label="logout-button"]').should('be.visible');
+		cy.get('[aria-label="logout-button"]>span').should('contain', 'Log Out');
+		cy.viewport('iphone-x');
+		cy.get('[aria-label="logout-button-mobile"]').should('be.visible');
+	});
+	
+	it('should log out the user', () => {
+		cy.get('[aria-label="logout-button"]').should('be.visible');
+		cy.get('[aria-label="logout-button"]').click()
+		cy.url().should('include', '/login');
+	});
 })
